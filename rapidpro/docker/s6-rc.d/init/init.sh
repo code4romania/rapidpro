f73a548bff3abb0670a67bfe39a08ed19e86effb
Andrei Ioniță <hi@andrei.io>
2023-03-03 14:24:07 +0000

    wip rapidpro

---
+++ rapidpro/docker/s6-rc.d/init/init.sh
@@ -0,0 +1,31 @@
+#!/command/with-contenv sh
+
+cd /var/www/rapidpro
+
+echo "Checking..."
+python3 manage.py check
+
+if [ "${RUN_MIGRATION}" = "yes" ]; then
+    echo "Migrating databse"
+    python3 manage.py migrate --run-syncdb
+fi
+
+# if [ "${RUN_COMPILE_MESSAGES}" = "yes" ]; then
+#     echo "Compiling translation messages"
+#     python3 manage.py compilemessages
+# fi
+
+if [ "${RUN_COLLECT_STATIC}" = "yes" ]; then
+    echo "Collect static"
+    mkdir -p /var/www/rapidpro/sitestatic
+    python3 manage.py collectstatic --noinput
+fi
+
+if [ "${RUN_CREATE_SUPER_USER}" = "yes" ]; then
+    echo "Create superuser"
+    python3 manage.py createsuperuser --noinput \
+        --username "${DJANGO_ADMIN_USERNAME}" --email "${DJANGO_ADMIN_EMAIL}"
+
+    echo "Set superuser password"
+    python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.get(username=\"${DJANGO_ADMIN_USERNAME}\"); u.set_password(\"${DJANGO_ADMIN_PASSWORD}\"); u.save()"
+fi
---
+++ rapidpro/docker/settings.py
@@ -0,0 +1,99 @@
+import os
+import warnings
+from django.utils.translation import gettext_lazy as _
+
+from .settings_common import *  # noqa
+
+
+ADMINS=()
+
+EMAIL_HOST = os.getenv("EMAIL_HOST", "localhost")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "server@temba.io")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "mypassword")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "server@temba.io")
+EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 10
+
+SECRET_KEY = os.getenv("SECRET_KEY", "your secret key")
+
+_default_database_config = {
+    "ENGINE": "django.contrib.gis.db.backends.postgis",
+    "NAME": os.getenv("POSTGRES_DB","temba"),
+    "USER": os.getenv("POSTGRES_USER","temba"),
+    "PASSWORD": os.getenv("POSTGRES_PASSWORD","temba"),
+    "HOST": os.getenv("POSTGRES_HOSTNAME","localhost"),
+    "PORT": os.getenv("POSTGRES_PORT","5432"),
+    "ATOMIC_REQUESTS": True,
+    "CONN_MAX_AGE": 60,
+    "OPTIONS": {},
+    "DISABLE_SERVER_SIDE_CURSORS": True,
+}
+DATABASES = {"default": _default_database_config, "readonly": _default_database_config.copy()}
+
+DOMAIN_NAME = os.getenv("DOMAIN_NAME","example.com")
+
+STORAGE_URL = f"http://{DOMAIN_NAME}/media"
+BRANDING = {
+    DOMAIN_NAME: {
+        "slug": "rapidpro",
+        "name": "RapidPro",
+        "org": "UNICEF",
+        "colors": dict(primary="#0c6596"),
+        "styles": ["brands/rapidpro/font/style.css"],
+        "default_plan": TOPUP_PLAN,
+        "welcome_topup": 1000,
+        "email": "join@rapidpro.io",
+        "support_email": "noreply@example.com",
+        "link": f"https://{DOMAIN_NAME}",
+        "api_link": f"https://{DOMAIN_NAME}",
+        "docs_link": "http://docs.rapidpro.io",
+        "domain": DOMAIN_NAME,
+        "ticket_domain": "tickets.rapidpro.io",
+        "favico": "brands/rapidpro/rapidpro.ico",
+        "splash": "brands/rapidpro/splash.jpg",
+        "logo": "brands/rapidpro/logo.png",
+        "allow_signups": True,
+        "flow_types": ["M", "V", "B", "S"],  # see Flow.FLOW_TYPES
+        "location_support": True,
+        "tiers": dict(multi_user=0, multi_org=0),
+        "bundles": [],
+        "welcome_packs": [dict(size=5000, name="Demo Account"), dict(size=100000, name="UNICEF Account")],
+        "title": _("Visually build nationally scalable mobile applications"),
+        "description": _("Visually build nationally scalable mobile applications from anywhere in the world."),
+        "credits": "Copyright &copy; 2012-2022 UNICEF, Nyaruka. All Rights Reserved.",
+        "support_widget": False,
+    }
+}
+DEFAULT_BRAND = os.environ.get("DEFAULT_BRAND", DOMAIN_NAME)
+
+ALLOWED_HOSTS = ["*", f".{DOMAIN_NAME}"]
+
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = 6379
+REDIS_DB = 10 if TESTING else 15  # we use a redis db of 10 for testing so that we maintain caches for dev
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://%s:%s/%s" % (REDIS_HOST, REDIS_PORT, REDIS_DB),
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+    }
+}
+
+INTERNAL_IPS = ("127.0.0.1",)
+
+MAILROOM_URL = os.environ.get("MAILROOM_URL", "http://localhost:8090")
+MAILROOM_AUTH_TOKEN = os.environ.get("MAILROOM_AUTH_TOKEN", "")
+
+INSTALLED_APPS = INSTALLED_APPS + ("storages", "tembaimporter", )
+
+MIDDLEWARE = ("temba.middleware.ExceptionMiddleware",) + MIDDLEWARE
+
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
+
+warnings.filterwarnings(
+    "error", r"DateTimeField .* received a naive datetime", RuntimeWarning, r"django\.db\.models\.fields"
+)
+
+STATIC_URL = "/sitestatic/"
