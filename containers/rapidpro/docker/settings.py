ea4819f6b9e280e1c0c6b8568016fbae7b0a6da7
Andrei Ioniță <hi@andrei.io>
2023-03-07 13:00:16 +0000

    wip

--- containers/rapidpro/docker/settings.py
+++ containers/rapidpro/docker/settings.py
@@ -28,7 +28,7 @@ _default_database_config = {
     "OPTIONS": {},
     "DISABLE_SERVER_SIDE_CURSORS": True,
 }
-DATABASES = {"default": _default_database_config, "readonly": _default_database_config.copy()}
+DATABASES = {"default": _default_database_config}
 DEBUG = os.getenv("DEBUG", "False").lower() in ('true', '1', 'yes')
 DOMAIN_NAME = os.getenv("DOMAIN_NAME","example.com")
 
