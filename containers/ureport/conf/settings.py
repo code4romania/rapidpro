50aa8a9cb99063d46d53d5b1c5ff834dcc6d23fe
Andrei Ioniță <hi@andrei.io>
2023-03-10 19:16:56 +0000

    wip

--- containers/ureport/conf/settings.py
+++ containers/ureport/conf/settings.py
@@ -33,7 +33,6 @@ SITE_API_HOST = os.getenv("RAPIDPRO_API_URL", 'https://api.rapidpro.io')
 
 HOSTNAME = os.getenv("HOSTNAME", "ureport.io")
 
-
 LOGGING['loggers']['celery.worker'] = {
     'level': 'DEBUG',
     'handlers': ['console'],
@@ -44,6 +43,7 @@ if os.getenv('AWS_STORAGE_BUCKET_NAME', False):
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', '')
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
