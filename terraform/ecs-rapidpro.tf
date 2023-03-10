5509e6d38266978ffc6b114a8b80acdd1fe58165
Andrei Ioniță <hi@andrei.io>
2023-03-10 22:45:43 +0000

    wip

--- terraform/ecs-rapidpro.tf
+++ terraform/ecs-rapidpro.tf
@@ -3,8 +3,8 @@ resource "aws_ecs_task_definition" "rapidpro" {
   execution_role_arn       = aws_iam_role.rapidpro_execution_role.arn
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "1024"
+  memory                   = "2048"
 
   container_definitions = jsonencode([
     {
