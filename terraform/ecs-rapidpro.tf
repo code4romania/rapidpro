29ddda2265d741ecb9c17742f05cfb0c0c8d695a
Andrei Ioniță <hi@andrei.io>
2023-03-10 18:55:28 +0000

    revert pgsql provider

--- terraform/ecs-rapidpro.tf
+++ terraform/ecs-rapidpro.tf
@@ -47,7 +47,7 @@ resource "aws_ecs_task_definition" "rapidpro" {
         },
         {
           name  = "POSTGRES_DB"
-          value = postgresql_database.rapidpro.name
+          value = aws_db_instance.db_instance.db_name
         },
         {
           name  = "POSTGRES_USER"
