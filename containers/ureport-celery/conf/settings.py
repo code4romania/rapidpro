0c8940ce0b58ea7c9f4991a287376e2fa5107342
Andrei Ioniță <hi@andrei.io>
2023-03-15 17:51:27 +0000

    add s3 custom domain

--- containers/ureport-celery/conf/settings.py
+++ containers/ureport-celery/conf/settings.py
@@ -44,6 +44,7 @@ if os.getenv('AWS_STORAGE_BUCKET_NAME', False):
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', '')
+    AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
