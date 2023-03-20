3adde28384f2695305c73a1fc3a07780da8fccac
Andrei Ioniță <hi@andrei.io>
2023-03-20 15:59:02 +0000

    wip

--- terraform/modules/ecs-cluster/locals.tf
+++ terraform/modules/ecs-cluster/locals.tf
@@ -1,4 +1,30 @@
-resource "aws_ecs_cluster" "main" {
-  name = local.namespace
-  tags = var.tags
+locals {
+  tags = concat(
+    [
+      {
+        key                 = "Name"
+        value               = "${var.name}-ecs-node"
+        propagate_at_launch = true
+      },
+      {
+        key                 = "AmazonECSManaged"
+        value               = "true"
+        propagate_at_launch = true
+      }
+    ],
+    var.asg_tags,
+    local.tags_asg_format,
+  )
+
+  tags_asg_format = null_resource.tags_as_list_of_maps.*.triggers
+}
+
+resource "null_resource" "tags_as_list_of_maps" {
+  count = length(keys(var.tags))
+
+  triggers = {
+    "key"                 = keys(var.tags)[count.index]
+    "value"               = values(var.tags)[count.index]
+    "propagate_at_launch" = "true"
+  }
 }
