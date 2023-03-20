55c8384603e24bdd74cd843d47998a1c5e531dda
Andrei Ioniță <hi@andrei.io>
2023-03-20 13:05:03 +0000

    reset

--- terraform/cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "rapidpro" {
-  name              = local.namespace
-  retention_in_days = 30
-}
--- terraform/random.tf
+++
@@ -1,6 +0,0 @@
-resource "random_string" "s3_bucket_suffix" {
-  length  = 8
-  special = false
-  upper   = false
-  numeric = false
-}
