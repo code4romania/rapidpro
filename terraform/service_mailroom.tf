1d046c585c0b0956bb403058b6cc65e96602741f
Andrei Ioniță <hi@andrei.io>
2023-03-21 15:37:51 +0000

    wip

---
+++ terraform/service_mailroom.tf
@@ -0,0 +1,214 @@
+module "ecs_mailroom" {
+  source = "./modules/ecs-service"
+
+  enable_execute_command = var.enable_execute_command
+
+  name                        = "mailroom"
+  cluster_name                = module.ecs_cluster.cluster_name
+  image_repo                  = data.aws_ecr_repository.mailroom.repository_url
+  image_tag                   = "edge"
+  container_port              = 8090
+  min_capacity                = 1
+  max_capacity                = 1
+  memory                      = 512
+  container_memory_soft_limit = 512
+  container_memory_hard_limit = 1024
+  predefined_metric_type      = "ECSServiceAverageCPUUtilization"
+  target_value                = 80
+  log_group_name              = module.ecs_cluster.log_group_name
+
+  ordered_placement_strategy = [
+    {
+      type  = "binpack"
+      field = "memory"
+    },
+    {
+      type  = "binpack"
+      field = "cpu"
+    },
+    {
+      type  = "spread"
+      field = "instanceId"
+    }
+  ]
+
+  environment = [
+    {
+      name  = "MAILROOM_DOMAIN"
+      value = local.domains.rapidpro
+    },
+    {
+      name  = "MAILROOM_ADDRESS"
+      value = "0.0.0.0"
+    },
+    {
+      name  = "MAILROOM_AUTH_TOKEN"
+      value = local.elasticache_url
+    },
+    {
+      name  = "MAILROOM_LOG_LEVEL"
+      value = "error"
+    },
+    {
+      name  = "MAILROOM_REDIS"
+      value = local.elasticache_url
+    },
+    {
+      name  = "MAILROOM_ELASTIC"
+      value = "https://${aws_opensearch_domain.main.endpoint}"
+    },
+    {
+      name  = "MAILROOM_S3_REGION"
+      value = var.region
+    },
+    {
+      name  = "MAILROOM_S3_ENDPOINT"
+      value = "https://s3.${var.region}.amazonaws.com"
+    },
+    {
+      name  = "MAILROOM_AWS_ACCESS_KEY_ID"
+      value = aws_iam_access_key.mailroom.id
+    },
+    {
+      name  = "MAILROOM_AWS_SECRET_ACCESS_KEY"
+      value = aws_iam_access_key.mailroom.secret
+    },
+    {
+      name  = "MAILROOM_SESSION_STORAGE"
+      value = "s3"
+    },
+    {
+      name  = "MAILROOM_S3_SESSION_BUCKET"
+      value = aws_s3_bucket.mailroom.bucket
+    },
+    {
+      name  = "MAILROOM_S3_SESSION_PREFIX"
+      value = "/sessions/"
+    },
+    {
+      name  = "MAILROOM_S3_MEDIA_BUCKET"
+      value = aws_s3_bucket.mailroom.bucket
+    },
+    {
+      name  = "MAILROOM_S3_MEDIA_PREFIX"
+      value = "/media/"
+    },
+  ]
+
+  secrets = [
+    {
+      name      = "MAILROOM_DB"
+      valueFrom = aws_secretsmanager_secret.rapidpro-db-url.arn
+    },
+    {
+      name      = "MAILROOM_AUTH_TOKEN"
+      valueFrom = aws_secretsmanager_secret.mailroom_auth_token.arn
+    },
+    {
+      name      = "MAILROOM_FCM_KEY"
+      valueFrom = aws_secretsmanager_secret.mailroom_fcm_key.arn
+    }
+  ]
+
+  allowed_secrets = [
+    aws_secretsmanager_secret.rapidpro-db-url.arn,
+    aws_secretsmanager_secret.mailroom_auth_token.arn,
+  ]
+}
+
+resource "random_password" "mailroom_auth_token" {
+  length  = 32
+  special = false
+
+  lifecycle {
+    ignore_changes = [
+      length,
+      special
+    ]
+  }
+}
+
+resource "aws_secretsmanager_secret" "mailroom_auth_token" {
+  name = "${local.namespace}-mailroom_auth_token"
+}
+
+resource "aws_secretsmanager_secret_version" "mailroom_auth_token" {
+  secret_id     = aws_secretsmanager_secret.mailroom_auth_token.id
+  secret_string = random_password.mailroom_auth_token.result
+}
+
+resource "aws_secretsmanager_secret" "mailroom_fcm_key" {
+  name = "${local.namespace}-mailroom_fcm_key"
+}
+
+resource "random_string" "mailroom_s3_bucket_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+  numeric = false
+}
+
+
+resource "aws_s3_bucket" "mailroom" {
+  bucket = "${local.namespace}-mailroom-${random_string.mailroom_s3_bucket_suffix.result}"
+}
+
+resource "aws_s3_bucket_ownership_controls" "mailroom" {
+  bucket = aws_s3_bucket.mailroom.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "mailroom_public_access_block" {
+  bucket = aws_s3_bucket.mailroom.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "mailroom" {
+  bucket = aws_s3_bucket.mailroom.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_iam_access_key" "mailroom" {
+  user = aws_iam_user.mailroom.name
+}
+
+resource "aws_iam_user" "mailroom" {
+  name = "${local.namespace}-mailroom-user"
+}
+
+data "aws_iam_policy_document" "mailroom_bucket_acccess" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:GetObjectAcl",
+      "s3:PutObjectAcl",
+      "s3:PutObject"
+    ]
+
+    resources = [
+      aws_s3_bucket.mailroom.arn,
+      "${aws_s3_bucket.mailroom.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "mailroom_access_policy" {
+  name   = "${local.namespace}-mailroom-s3-access-policy"
+  user   = aws_iam_user.mailroom.name
+  policy = data.aws_iam_policy_document.mailroom_bucket_acccess.json
+}
+
