d973e23ea6c4bf09865b801e3b84fcf186a04bf0
Andrei Ioniță <hi@andrei.io>
2023-03-31 17:41:28 +0100

    disable debug

--- terraform/service_courier.tf
+++ terraform/service_courier.tf
@@ -42,7 +42,7 @@ module "ecs_courier" {
     },
     {
       name  = "COURIER_LOG_LEVEL"
-      value = "debug"
+      value = "error"
     },
     {
       name  = "COURIER_SPOOL_DIR"
--- terraform/service_mailroom.tf
+++ terraform/service_mailroom.tf
@@ -38,7 +38,7 @@ module "ecs_mailroom" {
     },
     {
       name  = "MAILROOM_LOG_LEVEL"
-      value = "debug"
+      value = "error"
     },
     {
       name  = "MAILROOM_REDIS"
